5c64566e95b8a2c728eb9b009d3872f89bf2b80b
Drew Seewald <atseewal@umich.edu>
2020-01-11 23:13:06 -0500

    Add request logger filter

--- plumber.R
+++ plumber.R
@@ -45,3 +45,13 @@ function() {
 function() {
 	
 }
+
+#' Log information about the incoming requests
+#' @filter logger
+
+function(req) {
+	cat(as.character(Sys.time()), "-",
+	    req$REQUEST_METHOD, req$PATH_INFO, "-",
+	    req$HTTP_USER_AGENT, "@", req$REMOTE_ADDR, "\n")
+	plumber::forward()
+}
